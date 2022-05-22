ea8317975640c523ca923ec1b3fb245adc750a3f
braincee <kwesi938@gmail.com>
2022-05-22 12:31:10 +0100

    Add Services

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,22 +1,40 @@
-$( function () {
-   $( '#hero-slider' ).owlCarousel( {
-    loop:true,
-    margin:0,
-    nav: true,
-       items: 1,
-       dots: false,
-       smartSpeed: 1500,
-    navText: ['PREV', 'NEXT'],
-    responsive:{
-        0:{
-        
-        },
-        600:{
-            
-        },
-        1000:{
-            
+$( function ()
+{
+    $( '#hero-slider' ).owlCarousel( {
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
+        smartSpeed: 1500,
+        navText: [ 'PREV', 'NEXT' ],
+        responsive: {
+            0: {
+                nav: false,
+            },
+            768: {
+               nav: true,
+            },
         }
-     }
-    }) 
-})
+    } );
+    
+    $( '#project-slider' ).owlCarousel( {
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: false,
+        smartSpeed: 1500,
+        margin: 24,
+        responsive: {
+            0: {
+                
+            },
+            768: {
+            
+            },
+            1140: {
+               items: 2,
+            }
+        }
+    });
+});
